bc85be64f9d379b45eaa9a634dac06d56ca96a2f
Suvid Thungathurti <suvidthungathurti@MacBookAir.attlocal.net>
2025-05-05 23:06:00 -0400

    another commit

--- main.cpp
+++ main.cpp
@@ -1,11 +1,51 @@
 #include <iostream>
-#include <stdio.h>
+/*
+lets you use:
+- cin: input from keyboard
+- cout: output from screen
+- cerr: error messages
+- endl: newline + flushing the output buffer
+*/
+
+#include <fstream>
+/*
+lets you use:
+- ifstream: input file stream
+- ofstream: output file stream
+- fstream: file stream for both input and output
+*/
+
+#include <string>
+/*
+allows you to use the strings class
+the string class allows you to use instances of strings
+strings are like a list of chars
+*/
+
+#include <vector>
+/*
+
+*/
 
 using namespace std;
 
 int main() {
 
-    cout << "something";
+    string inputFile = "input.txt";
+    ifstream file;
+
+    file.open(inputFile);
+
+    if (!file.is_open()) {
+        cerr << "There was an error opening the file" << endl;
+    }
+
+    string line;
+    while (getline(file, line)) {
+
+    }
+
+    file.close();
 
     return 0;
 }
